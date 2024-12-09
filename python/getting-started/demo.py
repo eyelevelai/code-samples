9aa6ddfcd109694ae224e0a728afebb303f51f09
Daniel Warfield <danielwarfield1@gmail.com>
2024-12-09 14:30:32 -0600

    Copied code samples from GX
    given the general migration of GX to EL, and GX being galvanized as a product within EL, I think the code samples should exist here, and this one should be updated.

---
+++ python/getting-started/demo.py
@@ -0,0 +1,114 @@
+import os, time
+
+from groundx import Groundx, ApiException
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if os.getenv("GROUNDX_API_KEY") is None:
+    raise Exception(
+        """
+
+    You have not set a required environment variable (GROUNDX_API_KEY)
+    Copy .env.sample and rename it to .env then fill in the missing values
+"""
+    )
+
+query = "YOUR QUERY"
+
+# set to a value to skip a bucket lookup
+# otherwise this demo will use the first result from get all buckets
+bucketId = 0
+
+# enumerated file type (e.g. docx, pdf)
+# must be set to upload local or hosted
+# fileType = ""
+fileType = ""
+
+# must be set to upload local
+fileName = ""
+
+# set to local file path to upload local file
+uploadLocal = ""
+
+# set to hosted URL to upload hosted file
+uploadHosted = ""
+
+
+# initialize client
+groundx = Groundx(
+    api_key=os.getenv("GROUNDX_API_KEY"),
+)
+
+
+if bucketId == 0:
+    # list buckets
+    try:
+        bucket_response = groundx.buckets.list()
+
+        if len(bucket_response.body["buckets"]) < 1:
+            print(bucket_response.body["buckets"])
+            raise Exception("no results from buckets")
+
+        bucketId = bucket_response.body["buckets"][0]["bucketId"]
+    except ApiException as e:
+        print("Exception when calling BucketApi.list: %s\n" % e)
+
+
+if uploadLocal != "" and fileType != "" and fileName != "":
+    # upload local documents to GroundX
+    try:
+        ingest = groundx.documents.upload_local(
+            body=[
+                {
+                    "blob": open(uploadLocal, "rb"),
+                    "metadata": {
+                        "bucketId": bucketId,
+                        "fileName": fileName,
+                        "fileType": fileType,
+                    },
+                },
+            ]
+        )
+
+        while (
+            ingest.body["ingest"]["status"] != "complete"
+            and ingest.body["ingest"]["status"] != "error"
+            and ingest.body["ingest"]["status"] != "cancelled"
+        ):
+            time.sleep(3)
+            ingest = groundx.documents.get_processing_status_by_id(
+                process_id=ingest.body["ingest"]["processId"]
+            )
+    except ApiException as e:
+        print("Exception when calling DocumentApi.upload_local: %s\n" % e)
+
+
+if uploadHosted != "":
+    # upload hosted documents to GroundX
+    try:
+        ingest = groundx.documents.upload_remote(
+            documents=[{"bucketId": bucketId, "sourceUrl": uploadHosted}],
+        )
+
+        while (
+            ingest.body["ingest"]["status"] != "complete"
+            and ingest.body["ingest"]["status"] != "error"
+            and ingest.body["ingest"]["status"] != "cancelled"
+        ):
+            time.sleep(3)
+            ingest = groundx.documents.get_processing_status_by_id(
+                process_id=ingest.body["ingest"]["processId"]
+            )
+    except ApiException as e:
+        print("Exception when calling DocumentApi.upload_remote: %s\n" % e)
+
+if query != "":
+    # search
+    try:
+        content_response = groundx.search.content(id=bucketId, query=query)
+
+        print(content_response.body["search"]["text"])
+    except ApiException as e:
+        print("Exception when calling SearchApi.content: %s\n" % e)
---
+++ typescript/getting-started/demo.js
@@ -0,0 +1,145 @@
+import fs from 'fs';
+import { Groundx } from "groundx-typescript-sdk";
+
+import dotenv from 'dotenv'; 
+dotenv.config();
+
+if (!process.env.GROUNDX_API_KEY) {
+  throw Error("You have not set a required environment variable (GROUNDX_API_KEY or OPENAI_API_KEY). Copy .env.sample and rename it to .env then fill in the missing values.");
+}
+
+const query = "YOUR QUERY";
+
+// set to skip lookup, otherwise will be set to first result
+let bucketId = 0;
+
+// enumerated file type (e.g. docx, pdf)
+// must be set to upload local or hosted
+const fileType = "";
+
+// must be set to upload local
+const fileName = ""
+
+// set to local file path to upload local file
+const uploadLocal = "";
+
+// set to hosted URL to upload hosted file
+const uploadHosted = "";
+
+// initialize client
+const groundx = new Groundx({
+  apiKey: process.env.GROUNDX_API_KEY,
+});
+
+
+if (bucketId === 0) {
+  // list buckets
+  const bucketResponse = await groundx.buckets.list();
+  if (!bucketResponse || !bucketResponse.status || bucketResponse.status != 200 ||
+      !bucketResponse.data || !bucketResponse.data.buckets) {
+    console.error(bucketResponse);
+    throw Error("GroundX bucket request failed");
+  }
+
+  if (bucketResponse.data.buckets.count < 1) {
+    console.error("no results from buckets");
+    console.log(bucketResponse.data.buckets);
+    throw Error("no results from GroundX bucket query");
+  }
+
+  bucketId = bucketResponse.data.buckets[0].bucketId;
+}
+
+
+if (uploadLocal !== "" && fileType !== "" && fileName !== "") {
+  // upload local documents to GroundX
+  let ingest = await groundx.documents.uploadLocal([
+    {
+      blob: fs.readFileSync(uploadLocal),
+      metadata: {
+        bucketId: bucketId,
+        fileName: fileName,
+        fileType: fileType,
+      },
+    }
+  ]);
+
+  if (!ingest || !ingest.status || ingest.status != 200 ||
+    !ingest.data || !ingest.data.ingest) {
+    console.error(ingest);
+    throw Error("GroundX upload request failed");
+  }
+
+  // poll ingest status
+  while (ingest.data.ingest.status !== "complete" &&
+    ingest.data.ingest.status !== "error" &&
+    ingest.data.ingest.status !== "cancelled") {
+    ingest = await groundx.documents.getProcessingStatusById({
+      processId: ingest.data.ingest.processId,
+    });
+    if (!ingest || !ingest.status || ingest.status != 200 ||
+      !ingest.data || !ingest.data.ingest) {
+      console.error(ingest);
+      throw Error("GroundX upload request failed");
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+  }
+}
+
+if (uploadHosted !== "" && fileType !== "") {
+  // upload hosted documents to GroundX
+  let ingest = await groundx.documents.uploadRemote({
+    documents: [
+      {
+        bucketId: bucketId,
+        fileType: fileType,
+        sourceUrl: uploadHosted,
+      }
+    ]
+  });
+
+  if (!ingest || !ingest.status || ingest.status != 200 ||
+    !ingest.data || !ingest.data.ingest) {
+    console.error(ingest);
+    throw Error("GroundX upload request failed");
+  }
+
+  // poll ingest status
+  while (ingest.data.ingest.status !== "complete" &&
+    ingest.data.ingest.status !== "error" &&
+    ingest.data.ingest.status !== "cancelled") {
+    ingest = await groundx.documents.getProcessingStatusById({
+      processId: ingest.data.ingest.processId,
+    });
+    if (!ingest || !ingest.status || ingest.status != 200 ||
+      !ingest.data || !ingest.data.ingest) {
+      console.error(ingest);
+      throw Error("GroundX upload request failed");
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+  }
+}
+
+if (query !== "") {
+  // search
+  const searchResponse = await groundx.search.content({
+    id: bucketId,
+    query: query
+  });
+
+  if (!searchResponse || !searchResponse.status || searchResponse.status != 200 ||
+    !searchResponse.data || !searchResponse.data.search) {
+    console.error(searchResponse);
+    throw Error("GroundX search request failed");
+  }
+
+  if (!searchResponse.data.search.text) {
+    console.error("no results from search");
+    console.log(searchResponse.data.search);
+    throw Error("no results from GroundX search query");
+  }
+
+  console.log(searchResponse.data.search.text);
+}
